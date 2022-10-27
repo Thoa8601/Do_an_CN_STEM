6f74bf528086aab8aff2a11d5b0fc3aa0fc55ac8
Thoa8601 <116791784+Thoa8601@users.noreply.github.com>
2022-10-27 14:36:00 +0700

    Update Smart_Stick.ino

--- Smart_Stick.ino
+++ Smart_Stick.ino
@@ -1,1 +1,58 @@
+#include <Wire.h>
+#include "rgb_lcd.h"
+rgb_lcd lcd;
+
+const int trigPin = D6;
+const int echoPin = D5;
+
+
+//define sound velocity in cm/uS
+#define SOUND_VELOCITY 0.034
+#define CM_TO_INCH 0.393701
+
+long duration;
+float distanceCm;
+
+void setup() {
+  Serial.begin(9600); // Starts the serial communication
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  lcd.begin(16, 2);
+  lcd.setCursor(4, 0);
+  lcd.print("Gay TO");
+}
+
+void loop() {
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculate the distance
+  distanceCm = duration * SOUND_VELOCITY/2;
+   if (distanceCm >=400 )
+{
+  lcd.setCursor(0, 0); // Sets the location at which subsequent text written to the LCD will be displayed
+  lcd.print("Distance: "); // Prints string "Distance" on the LCD
+  lcd.print(distanceCm); // Prints the distance value from the sensor
+  lcd.print(" cm");
+  lcd.setCursor(0, 1);
+  lcd.print("  Ngoai pham vi ");
+  delay(10);
+}
+else   {
+
+  lcd.setCursor(0, 0); // Sets the location at which subsequent text written to the LCD will be displayed
+  lcd.print("Distance: "); // Prints string "Distance" on the LCD
+  lcd.print(distanceCm); // Prints the distance value from the sensor
+  lcd.print(" cm");
+  lcd.setCursor(0, 1);
+  lcd.print("  Binh   thuong");
+  delay(10);
+}
+}
 
